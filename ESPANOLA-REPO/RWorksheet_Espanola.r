160a19ff40623f6e892caa6d3c8affdf6915a5b9
Niel Andrew Callanga <andrew.nac81@gmail.com>
2022-09-26 09:50:10 +0800

    first commit

---
+++ ESPANOLA-REPO/RWorksheet_Espanola.r
@@ -0,0 +1,42 @@
+#JACKLORD ESPANOLA BSIT-2A
+
+#Using functions seq(), assign(), min(), max(), c(), sort(), sum(), filter()
+
+#1. Set up a vector named age consisting of 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37,
+#34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41.
+
+age <- c( 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37,
+          34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
+age
+
+# a. a.How many data points? 
+length(age)
+
+#b.Write the R code and its output.
+
+#2.Find the reciprocal of the values for age.
+#Write the R code and its output.
+reciprocal <- function(age) vec <- 1/age 
+rage <- reciprocal(age)
+rage
+
+#3.Assign also new_age <- c(age, 0, age). 
+#What happen to the new_age?
+new_age <-c (age, 0, age)
+new_age
+
+#4.Sort the values for age.
+#Write the R code and its output.
+sort(age)
+
+#5. Find the minimum and maximum value for age.
+#Write the R code and its output.
+min(age)
+max(age)
+
+#6.Set up a vector named data, consisting of 2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+#2.5, 2.3, 2.4, and 2.7.
+data_Vector <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, and 2.7.)
+#a. How many data points?
+
+
